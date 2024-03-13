4bc4b2856c82f4e5e4e1fa4db844354c891c7bbf
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-03-13 09:26:20 +0600

    Started the knowledge project with react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl font-bold text-[#131313]'>React Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
